9ab8c273bdd9bf51d81b366ff0fc3850330dc460
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-05-20 23:52:11 +0300

    Fixed run error with wrong entered name of city

--- Lab9/Tasks/exampleBot.py
+++ Lab9/Tasks/exampleBot.py
@@ -19,27 +19,31 @@ def cmd_city(message):
     bot.register_next_step_handler(send, city)
 
 def city(message): #основна робота з декількома варіантами в залежності від результату
-    bot.send_message(message.chat.id, 'Дізнаюсь погодні умови в місті {city}'.format(city=message.text))
-    data = message.text
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(data)
-    observation = mgr.weather_at_place(message.text)
-    w = observation.weather
-    t = w.temperature('celsius')['temp']
-    
-    answer = f"В місті {message.text} зараз {w.detailed_status} \n"
-    answer += f"Приблизна температура {round(t)} ℃\n\n"
- 
-    if t < 0:
-        answer += 'Зараз температура нижче нуля, одягайся тепліше!'
-        bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/Xv92ltSnupgAAAAd/gosling-sad-gosling.gif')
-    elif t < 15:
-        answer += 'Зараз прохолодно, варто тепліше одягтися!'
-        bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/tgLuPlO6kz8AAAAd/what-gosling.gif')
-    else:
-        answer += 'Зараз досить тепло, можна одягтися легко!'
-        bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/F1z461rxQdwAAAAd/gosling-barbie.gif')
+    try:
+        bot.send_message(message.chat.id, 'Дізнаюсь погодні умови в місті {city}'.format(city=message.text))
+        data = message.text
+        mgr = owm.weather_manager()
+        observation = mgr.weather_at_place(data)
+        w = observation.weather
+        t = w.temperature('celsius')['temp']
+        
+        answer = f"В місті {message.text} зараз {w.detailed_status} \n"
+        answer += f"Приблизна температура {round(t)} ℃\n\n"
+     
+        if t < 0:
+            answer += 'Зараз температура нижче нуля, одягайся тепліше!'
+            bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/Xv92ltSnupgAAAAd/gosling-sad-gosling.gif')
+        elif t < 15:
+            answer += 'Зараз прохолодно, варто тепліше одягтися!'
+            bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/tgLuPlO6kz8AAAAd/what-gosling.gif')
+        else:
+            answer += 'Зараз досить тепло, можна одягтися легко!'
+            bot.send_animation(message.chat.id, 'https://media1.tenor.com/m/F1z461rxQdwAAAAd/gosling-barbie.gif')
 
-    bot.send_message(message.chat.id, answer)
+        bot.send_message(message.chat.id, answer)
+        
+    except Exception as e:
+        print(f"Помилка: {e}")
+        bot.send_message(message.chat.id, "❌ Помилка! Можливо, ви ввели неправильну назву міста. Спробуйте ще раз.")
 
-bot.polling(none_stop=True) # необхідно, щоб бот не вимкнувся одразу 
+bot.polling(none_stop=True) # необхідно, щоб бот не вимкнувся одразу
